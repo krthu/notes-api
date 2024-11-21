4eea2745ef6f7884d3252057ec94888f04e86080
KT <thun.kristian@gmail.com>
2024-11-21 21:55:40 +0100

    Validation more robost

--- middleware/errorHandler.mjs
+++ middleware/errorHandler.mjs
@@ -5,6 +5,6 @@ export const errorHandler = {
     onError: async (request) => {
         console.error('Error', request.event.error)
         const errorCode = request.event.errorCode ? request.event.errorCode : 400;
-        return sendResponse(errorCode, request.event.error)
+        return sendResponse(errorCode, {success: false, message: request.event.error})
     }
 }
