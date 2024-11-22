f72ad1ada9534856aee4bceb7e52af10463266b8
KT <thun.kristian@gmail.com>
2024-11-22 09:29:33 +0100

    Added objekt to validate notes to be reused did the same for login user and create user

--- middleware/jsonParsing.mjs
+++ middleware/jsonParsing.mjs
@@ -7,7 +7,7 @@ export const jsonParsing = {
             return request.response;
             
         } catch (error) {
-            request.event.error = {success: false, message: 'Error parsing json'};
+            request.event.error =  'Error parsing json';
             request.event.errorCode = 400;
             throw new Error();
         }
